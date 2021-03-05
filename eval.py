4d313775ec934dc6ad142b1cdf79e5838b493a7c
resteves-99 <rafaele675@gmail.com>
2021-03-04 21:13:39 -0800

    download test celeba

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
-from torchvision.datasets import MNIST
+from torchvision.datasets import CelebA
 import os
 
+train_dataset = CelebA('./data', split = 'test', transform=img_transform, download=True, target_type=None)
